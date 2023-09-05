13aaeb2cc4b7f4efd1b0595642ebfb18849c2e85
Victor Hugo <hey.zooi@gmail.com>
2023-09-05 00:37:41 -0400

    Stage 1 - Step 3

--- src/models.rs
+++ src/models.rs
@@ -1,19 +1,20 @@
 use std::collections::HashMap;
+use anyhow::{anyhow, Result};
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub enum Status {
     Open,
     InProgress,
     Closed,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct Epic {
     pub name: String,
     pub description: String,
     pub status: Status,
-    pub stories: Vec<usize>,
+    pub stories: Vec<u32>,
 }
 
 impl Epic {
@@ -27,7 +28,7 @@ impl Epic {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct Story {
     pub name: String,
     pub description: String,
@@ -44,11 +45,11 @@ impl Story {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct DBState {
-    pub last_item_id: usize,
-    pub epics: HashMap<i32, Epic>,
-    pub stories: HashMap<i32, Story>,
+    pub last_item_id: u32,
+    pub epics: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>,
 }
 
 impl DBState {
@@ -60,8 +61,55 @@ impl DBState {
         }
     }
 
-    fn generate_new_id(&mut self) -> usize {
-        self.last_item_id += 1;
+    fn next_id(&self) -> u32 {
+        self.last_item_id + 1
+    }
+
+    fn generate_new_id(&mut self) -> u32 {
+        self.last_item_id = self.next_id();
         self.last_item_id
+    }
+
+    pub fn create_epic(&mut self, epic: Epic) -> Result<u32> {
+        let id = self.generate_new_id();
+        self.epics.insert(id, epic);
+        Ok(id)
+    }
+
+    pub fn delete_epic(&mut self, epic_id: &u32) -> Result<Epic> {
+        let epic = self.epics.remove(epic_id).ok_or(anyhow!("Epic {epic_id} not found"))?;
+        for story_id in &epic.stories {
+            self.stories.remove(story_id);
+        }
+        Ok(epic)
+    }
+
+    pub fn update_epic_status(&mut self, epic_id: &u32, status: Status) -> Result<&Epic> {
+        let epic = self.epics.get_mut(epic_id).ok_or(anyhow!("Epic {epic_id} not found"))?;
+        epic.status = status;
+        Ok(epic)
+    }
+
+    pub fn create_story(&mut self, epic_id: &u32, story: Story) -> Result<u32> {
+        let id = self.next_id();
+        let epic = self.epics.get_mut(epic_id).ok_or(anyhow!("Epic {epic_id} not found"))?;
+        epic.stories.push(id);
+        self.stories.insert(id, story);
+        assert_eq!(id, self.generate_new_id());
+        Ok(id)
+    }
+
+    pub fn delete_story(&mut self, epic_id: &u32, story_id: &u32) -> Result<Story> {
+        let epic = self.epics.get_mut(epic_id).ok_or(anyhow!("Epic {epic_id} not found"))?;
+        epic.stories.sort();
+        let index = epic.stories.binary_search(story_id).map_err(|_| anyhow!("Story {story_id} not found"))?;
+        epic.stories.remove(index);
+        self.stories.remove(story_id).ok_or(anyhow!("Story {story_id} not found"))
+    }
+    
+    pub fn update_story_status(&mut self, story_id: &u32, status: Status) -> Result<&Story> {
+        let story = self.stories.get_mut(story_id).ok_or(anyhow!("Story {story_id} not found"))?;
+        story.status = status;
+        Ok(story)
     }
 }
